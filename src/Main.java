230ecd513dced524c8094756159e5fc6fba25a6d
Vladimir Khokhlov <vladimirkhokhlov@yandex.ru>
2022-09-02 14:44:15 +0700

    Class PersonComparator replaced to lambda expression

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,37 @@ public class Main {
             System.out.println(person);
         }
 
-        Collections.sort(list, new PersonComparator(2));
+        Comparator<Person> personComparator = (o1, o2) -> {
+            final int maxWords = 2;
+            int wordsInSurname1 = o1.getSurname().split("[ -]").length;
+            int wordsInSurname2 = o2.getSurname().split("[ -]").length;
+            int age1 = o1.getAge();
+            int age2 = o2.getAge();
+
+            if (wordsInSurname1 >= maxWords && wordsInSurname2 >= maxWords) {
+                if (age1 > age2) {
+                    return 1;
+                }
+                if (age1 == age2) {
+                    return 0;
+                }
+                return -1;
+            }
+
+            if (wordsInSurname1 > wordsInSurname2) {
+                return 1;
+            }
+            if (wordsInSurname1 == wordsInSurname2 && age1 > age2) {
+                return 1;
+            }
+            if (wordsInSurname1 == wordsInSurname2 && age1 == age2) {
+                return 0;
+            }
+
+            return -1;
+        };
+
+        Collections.sort(list, personComparator);
 
         System.out.println("\nПосле сортировки:");
 
