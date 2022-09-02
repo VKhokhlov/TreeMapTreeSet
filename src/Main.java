83cc868167bb5362a46ab462b15b38531b3b88d9
Vladimir Khokhlov <vladimirkhokhlov@yandex.ru>
2022-09-02 15:16:23 +0700

    Removed persons with age < 18

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -7,18 +8,22 @@ public class Main {
         list.add(new Person("Алексей", "Орлов-Чесменский", 35));
         list.add(new Person("Александр", "Суворов-Рымникский", 32));
         list.add(new Person("Анна", "Богалий-Титовец", 30));
-        list.add(new Person("Иван", "Иванов", 20));
-        list.add(new Person("Мария", "де Лос Анхелес Дюран де Лемос", 25));
+        list.add(new Person("Иван", "Иванов", 16));
+        list.add(new Person("Мария", "де Лос Анхелес Дюран де Лемос", 15));
 
-        System.out.println("До сортировки:");
+        printList("До удаления (18-):", list);
 
-        for (Person person : list) {
-            System.out.println(person);
-        }
+        list.removeIf((person) -> person.getAge() < 18);
+
+        printList("\nПосле удаления (18-):", list);
 
         Collections.sort(list, new PersonComparator(2));
 
-        System.out.println("\nПосле сортировки:");
+        printList("\nПосле сортировки:", list);
+    }
+
+    public static void printList(String msg, List<Person> list) {
+        System.out.println(msg);
 
         for (Person person : list) {
             System.out.println(person);
